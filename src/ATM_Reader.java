92447d2220eef43db2f60e20c104818d54d3cdbd
MrBhuvi5 <bhuvanesh14014@gmail.com>
2026-02-19 17:13:04 +0530

    version 2 - ATM Reader

--- src/ATM_Reader.java
+++ src/ATM_Reader.java
@@ -3,145 +3,98 @@ import java.sql.*;
 public class ATM_Reader {
 	public static void main(String[] args) throws Exception{
 		Scanner s=new Scanner(System.in);
-		
+		 //DB Connection
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/atm","root","12345678");
-		
-		System.out.println("Available ATMs \n1. IOB - Indian Oversease Bank "
-				+ "\n2. ICIC - Industrial Credit and Investment Corporation of India"+
-				"\n3. cnrb - Canara Bank"+"\n4. SBI - State Bank of India");
+		//User Choice
+		System.out.println("1. Find ATM ID \n2. Enter ATM ID");
 		System.out.print("Enter your choice: ");
-		int choice=s.nextInt();
+		int userChoice=s.nextInt();
+		s.nextLine();
 		System.out.println();
+		//Find ATM
+		if(userChoice==1) {
+			System.out.print("Enter you location: ");
+			String location=s.nextLine();
 		
-		switch(choice) {
-			case 1:	
-				iob(s,con);
-				break;
-			case 2:
-				icic(s,con);
-				break;
-			case 3:
-				cnrb(s,con);
-				break;
-			case 4:
-				sbi(s,con);
-				break;
-			case 5:
-				cub(s,con);
-				break;
-			default:
-				System.out.print("Enter a valid choice");
+			System.out.println("Available ATMs \n1. IOB - Indian Oversease Bank "
+					+ "\n2. ICIC - Industrial Credit and Investment Corporation of India"+
+					"\n3. cnrb - Canara Bank"+"\n4. SBI - State Bank of India");
+			System.out.print("Enter your choice: ");
+			int choice=s.nextInt();
+			
+			if(choice<1 && choice>4) {
+				System.out.println("Invalid Choice :(");
+				return;
+			}
+			atmByLocation(s,con,choice,location);
 		}
-		
+		//Find Avl_balance
+		else if(userChoice==2) {
+			System.out.print("Enter the AMT ID: ");
+			String atmID=s.next().toLowerCase();
+			System.out.println();
+			atmStatus(s,con,atmID);
+		}
 		
 		con.close();
 	}
 	
-	static void iob(Scanner s,Connection con) throws Exception{
+	static void atmByLocation(Scanner s,Connection con,int choice,String location) throws Exception{
 		
-		PreparedStatement ps=con.prepareStatement("Select * from iob where atm_id=?");
-		System.out.print("Enter the ATM ID: ");
-		String atmId=s.next();
-		ps.setString(1,atmId);
+		String query=null;
+		if(choice==1)
+			query="select * from iob where area like ? or city like ?  order by area asc";
+		else if(choice==2)
+			query="select * from icic where area like ? or city like ?  order by area asc";
+		else if(choice==3)
+			query="select * from cnrb where area like ? or city like ?  order by area asc";
+		else
+			query="select * from sbi where area like ? or city like ?  order by area asc";
+		
+		PreparedStatement ps=con.prepareStatement(query);
+		String temp="iob";
+		
+		
+		ps.setString(1,("%"+location+"%"));
+		ps.setString(2,("%"+location+"%"));
+		ResultSet rs=ps.executeQuery();
+		
+		while(rs.next()) {
+			System.out.print(("\n"+rs.getString("atm_id"))+" | "+rs.getString("branch_name")+" | "
+					+rs.getString("area")+" | "+rs.getString("city")+"\n");
+		}
+		
+	}
+	
+	static void atmStatus(Scanner s,Connection con,String atmID) throws Exception{
+		String query=null;
+		
+		if(atmID.contains("iob"))
+			query=("select * from iob where atm_id= ?");
+		else if(atmID.contains("icic")) 
+			query=("select * from icic where atm_id= ?");
+		else if(atmID.contains("cnrb")) 
+			query=("select * from cnrb where atm_id= ?");
+		else if(atmID.contains("sbi")) 
+			query=("select * from sbi where atm_id= ?");
+		else
+				System.out.println("Invalid ATM ID :( ");
+		
+		PreparedStatement ps=con.prepareStatement(query);
+		ps.setString(1,atmID);
+		
 		ResultSet rs=ps.executeQuery();
 		
 		if(rs.next()) {
-			System.out.print(("\n"+rs.getString("atm_id"))+" | "+rs.getString("branch_name")+" | "+rs.getString("location")+"\n");
-			
-			if(rs.getDouble("avl_balance")>100000) 
-				System.out.println("Available balance: 100000+");
+			System.out.print(rs.getString("atm_id")+" | "+rs.getString("branch_name")+" | "
+					+rs.getString("area")+" | "+rs.getString("city")+"\n");
+			if(rs.getDouble("avl_balance")>100000)
+				System.out.println("Available balance: 100000+");			
 			else
-				System.out.print("Available banance: "+rs.getString("avl_balance"));
+				System.out.println("Available balance: "+rs.getDouble("avl_balance"));
 		}
-		else {
-			System.out.println("Invalid ATM ID :(");
-		}
-	}
-	
-	
-	static void icic(Scanner s,Connection con) throws Exception{
 		
-		PreparedStatement ps=con.prepareStatement("select * from icic where atm_id=?");
-		System.out.print("Enter the ATM ID: ");
-		String atmId=s.next();
-		ps.setString(1,atmId);
-		ResultSet rs=ps.executeQuery();
-		
-		if(rs.next()) {
-			System.out.print(("\n"+rs.getString("atm_id"))+" | "+rs.getString("branch_name")+" | "+rs.getString("location")+"\n");
-			
-			if(rs.getDouble("avl_balance")>100000) 
-				System.out.println("Available balance: 100000+");
-			else
-				System.out.print("Available banance: "+rs.getString("avl_balance"));
-		}
-		else {
-			System.out.println("Invalid ATM ID");
-		}
 	}
-	
-	static void cnrb(Scanner s,Connection con) throws Exception{
-			
-			PreparedStatement ps=con.prepareStatement("select * from cnrb where atm_id=?");
-			System.out.print("Enter the ATM ID: ");
-			String atmId=s.next();
-			ps.setString(1,atmId);
-			ResultSet rs=ps.executeQuery();
-			
-			if(rs.next()) {
-				System.out.print(("\n"+rs.getString("atm_id"))+" | "+rs.getString("branch_name")+" | "+rs.getString("location")+"\n");
-				
-				if(rs.getDouble("avl_balance")>100000) 
-					System.out.println("Available balance: 100000+");
-				else
-					System.out.print("Available banance: "+rs.getString("avl_balance"));
-			}
-			else {
-				System.out.println("Invalid ATM ID");
-			}
-		}
-	
-	static void sbi(Scanner s,Connection con) throws Exception{
-			
-			PreparedStatement ps=con.prepareStatement("select * from sbi where atm_id=?");
-			System.out.print("Enter the ATM ID: ");
-			String atmId=s.next();
-			ps.setString(1,atmId);
-			ResultSet rs=ps.executeQuery();
-			
-			if(rs.next()) {
-				System.out.print(("\n"+rs.getString("atm_id"))+" | "+rs.getString("branch_name")+" | "+rs.getString("location")+"\n");
-				
-				if(rs.getDouble("avl_balance")>100000) 
-					System.out.println("Available balance: 100000+");
-				else
-					System.out.print("Available banance: "+rs.getString("avl_balance"));
-			}
-			else {
-				System.out.println("Invalid ATM ID");
-			}
-		}
-	
-	static void cub(Scanner s,Connection con) throws Exception{
-			
-			PreparedStatement ps=con.prepareStatement("select * from cub where atm_id=?");
-			System.out.print("Enter the ATM ID: ");
-			String atmId=s.next();
-			ps.setString(1,atmId);
-			ResultSet rs=ps.executeQuery();
-			
-			if(rs.next()) {
-				System.out.print(("\n"+rs.getString("atm_id"))+" | "+rs.getString("branch_name")+" | "+rs.getString("location")+"\n");
-				
-				if(rs.getDouble("avl_balance")>100000) 
-					System.out.println("Available balance: 100000+");
-				else
-					System.out.print("Available banance: "+rs.getString("avl_balance"));
-			}
-			else {
-				System.out.println("Invalid ATM ID");
-			}
-		}
 	
 }
